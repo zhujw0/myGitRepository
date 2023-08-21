5adadcdc309a9f17b938c96c03fa630222cfa987
wei <1506839715@qq.com>
2023-08-22 01:15:17 +0800

    第二次提交

--- src/main/java/socketTest/udp/UdpSocket.java
+++ src/main/java/socketTest/udp/UdpSocket.java
@@ -3,7 +3,7 @@ package socketTest.udp;
 public class UdpSocket {
 
     public static void main(String[] args) {
-        System.out.println("4444444444fr4f");
+        System.out.println("444fr4f");
     }
 
 
